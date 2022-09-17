f110b88180353b2e9f294c2b3d9a790b6b5bc7b6
tochievol <tochievol@gmail.com>
2022-09-16 22:36:44 -0700

    nestedb loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-*_islower - check for lowercase character
-*@c: character to check the case
+*_islower - checks for lowercase character
+*@c: the character to check
 *Return: 1 if c is loweracase, 0 otherwise
 */
 int _islower(int c)
 {
-return (c >= "a" && c <= 'z');
+return (c >= 'a' && c <= 'z');
 }

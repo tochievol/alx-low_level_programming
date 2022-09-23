ff73eea3495068f2ec85c02261d6afab024b0f32
tochievol <tochievol@gmail.com>
2022-09-23 14:59:24 -0700

    C- Pointers, arrays and strings C options

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,17 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
+int i = 0;
 	
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (i >= 0)
-	{
-		_putchar(s[i]);
-		i--;
-	}
-	_putchar('\n');
+while (s[i] != '\0')
+{
+i++;
+}
+i--;
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
 }

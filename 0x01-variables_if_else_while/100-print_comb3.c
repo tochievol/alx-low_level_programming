055811e98a8f04ffb5072d81f10741515ff7f61f
tochievol <tochievol@gmail.com>
2022-09-23 11:35:10 -0700

    Vairable

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,12 +2,12 @@
 
 /**
 * main - main block
-*
 * Description: Print all possible combinations of two digits.
 * Numbers must be separated by commas and a space.
 * 01 and 10 are considered as the same combination of the two digits.
 * Print only the smallest combination of two digits.
-* Numbers should be printed in ascending order, with two digits* You can only use `putchar`.
+* Numbers should be printed in ascending order, with two digits
+* You can only use `putchar`.
 * You can only use `putchar` up to 5 times.
 * You are not allowed to use any variable of type `char`.
 * Return: 0
@@ -25,7 +25,8 @@ if (k < j)
 putchar(k + '0');
 putchar(j + '0');
 if (i < 89)
-}putchar(44);
+{
+putchar(44);
 putchar(32)
 }
 }

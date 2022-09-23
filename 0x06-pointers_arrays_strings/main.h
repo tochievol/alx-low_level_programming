8aa7e0d8d7cf186d2aa5ef14e98682209e2c9a59
tochievol <tochievol@gmail.com>
2022-09-23 12:41:05 -0700

    More C function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,21 +1,21 @@
-#include "main.h"
+int _putchar(char c);
 
-/**
-* _strcmp - compares two strings
-* @s1: first string.
-* @s2: second string.
-* Return: 0 if s1 and s2 are equals,
-* another number if not.
-*/
-int _strcmp(char *s1, char *s2)
-{
-int i = 0, op = 0;
-while (op == 0)
-{
-if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-break;
-op = *(s1 + i) - *(s2 + i);
-i++;
-}
-return (op);
-}
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *s);
+
+char *cap_string(char *s);
+
+char *leet(char *s);
+
+char *rot13(char *);
+
+void print_number(int n);

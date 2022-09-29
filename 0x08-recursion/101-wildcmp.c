1f24ad8341536e003e400a0849b8b57bef143873
tochievol <tochievol@gmail.com>
2022-09-29 08:02:20 -0700

    Task on Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,6 +6,7 @@
  * @s2: pointer to string params
  * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
@@ -16,13 +17,14 @@ int wildcmp(char *s1, char *s2)
 		}
 		return (*s2 == '\0');
 	}
+
 	if (*s2 == '*')
-}
-return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-}
-else if (*s1 == *s2)
-{
-	return (wildcmp(s1 + 1, s2 + 1));
-}
-return (0);
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }

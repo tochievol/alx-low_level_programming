37b5b1ba5c6ff7e15aa0f423d32121fb8839bc4f
tochievol <tochievol@gmail.com>
2022-09-29 06:38:24 -0700

    recurssion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@ int _strlen_recursion(char *s)
 {
 	if (!*s)
 	{
-		return (0);
+	return (0);
 	}
 	return (1 + _strlen_recursion(++s));
 }
@@ -23,11 +23,11 @@ int p1(char *s, int l)
 {
 	if (l < 1)
 	{
-		return (1);
+	return (1);
 	}
 	if (*s == *(s + l))
 	{
-		return (p1(s + 1, l - 2));
+	return (p1(s + 1, l - 2));
 	}
 	return (0);
 }

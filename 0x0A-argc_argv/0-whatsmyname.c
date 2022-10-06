c30e0813631d3ba1b74a2a0635d8dd2fe94339b7
tochievol <tochievol@gmail.com>
2022-10-06 02:07:20 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - writes programe name
+ * @arg: argument count
+ * @argv: array of pointers to CLI argument
+ * Return: 0
+ * **/
+int main(int argc,char *argv[])
+{
+	print("%\n", argv[argc- 1]);
+			return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc, char *argv[])
+	int main(int argc, __attribute__((unused)) char *argv[])
+	int main(int argc, char *argv[])
+	int main(int argc, char  *argv[])
+	int isInteger(const char *s)
+	int main(int argc, char const *argv[]

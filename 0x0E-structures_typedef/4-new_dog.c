39f7e02ddfb4a04afa20dc5b3fc7c8105deb895c
tochievol <tochievol@gmail.com>
2022-10-20 14:33:34 -0700

    Structure Types

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,70 +1,83 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
- * new_dog - creates a new dog.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: owner of the dog.
- *
- * Return: struct dog.
- * if fails, returns NULL.
+ * _copy  -   Make a copy of passed in argument
+ * @src:      Data to make copy of
+ * Return:    Pointer
  */
-dog_t *new_dog(char *name, float age, char *owner)
+
+char *_copy(char *src)
 {
-	dog_t *p_dog;
-	int i, lname, lowner;
-	
-	p_dog = malloc(sizeof(*p_dog));
-	if (p_dog == NULL || !(name) || !(owner))
+	char *ptr;
+	int i, len;
+
+	if (src == NULL)
 	{
-		free(p_dog);
 		return (NULL);
 	}
 
-	for (lname = 0; name[lname]; lname++)
-		;
-	for (lowner = 0; owner[lowner]; lowner++)
+	for (len = 0; src[len] != '\0'; len++)
 		;
 
-	p_dog->name = malloc(lname + 1);
-	p_dog->owner = malloc(lowner + 1);
+	ptr = malloc(sizeof(char) * (len + 1));
 
-	if (!(p_dog->name) || !(p_dog->owner)
-			{
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 
-															free(p_dog->owner);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		ptr[i] = src[i];
+	}
 
-																	free(p_dog->name);
+	ptr[i] = '\0';
+	return (ptr);
+}
 
-																			free(p_dog);
+/**
+ * new_dog     - Create a new dog variable
+ * @name:        Name of the dog
+ * @age:         Age of the dog
+ * @owner:       Owner of the dog
+ * Return:       Pointer to new dog variable
+ */
 
-																					return (NULL);
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *snoopie;
+	char *new_name, *new_owner;
 
-																						}
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 
+	snoopie = malloc(sizeof(dog_t));
+	if (snoopie == NULL)
+	{
+		return (NULL);
+	}
 
+	new_name = _copy(name);
+	if (new_name == NULL)
+	{
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).name = new_name;
 
-											for (i = 0; i < lname; i++)
+	(*snoopie).age = age;
 
-														p_dog->name[i] = name[i];
+	new_owner = _copy(owner);
+	if (new_owner == NULL)
+	{
+		free((*snoopie).name);
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).owner = new_owner;
 
-												p_dog->name[i] = '\0';
-
-
-
-													p_dog->age = age;
-
-
-
-														for (i = 0; i < lowner; i++)
-
-																	p_dog->owner[i] = owner[i];
-
-															p_dog->owner[i] = '\0';
-
-
-
-																return (p_dog);
-
+	return (snoopie);
 }

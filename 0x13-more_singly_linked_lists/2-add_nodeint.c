fc214d60fe6447576c71394124ae6596252addac
tochievol <tochievol@gmail.com>
2022-10-25 03:39:57 -0700

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-	new->=n;
+	new->n = n;
 	new->next = *head;
 	*head = new;
 
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,7 +19,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	
 	if (new == NULL)
 		return (NULL);
-	
+
 	new->n = n;
 	new->next = NULL;
 	temp = *head;

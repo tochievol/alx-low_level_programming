9f12e3b8893e6c1bef692d4249296c7e9413c55d
tochievol <tochievol@gmail.com>
2022-10-25 14:53:10 -0700

    Argc Argv

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,12 +23,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		else
 			printf("(nil)");
-		
+
 		if (i < n - 1)
 			if (separator)
 				printf("%s", separator);
 	}
-	
+
 	printf("\n");
 	va_end(valist);
 }

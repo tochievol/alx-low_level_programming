b26a7f582240639d4584adb0c1a72da35e966c7f
tochievol <tochievol@gmail.com>
2022-10-25 14:25:19 -0700

    Argc Argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,10 @@ int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 	}
 	else
-		printf("Error\n");
+	printf("Error\n");
 	return (1);
 }

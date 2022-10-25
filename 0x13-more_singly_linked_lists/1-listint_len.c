f91d4f3887755172e51d6fbd6a355c63ba4cab00
tochievol <tochievol@gmail.com>
2022-10-25 03:24:27 -0700

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,7 +10,7 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t nnodes = 0;
-	
+
 	while (h != NULL)
 	{
 		h = h->next;
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,17 +10,16 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-
 	listint_t *new;
-	
-	new = malloc(sizeof(listint_t));
-	
+
+	new = malloc(sizeof(listint_it));
+
 	if (new == NULL)
 		return (NULL);
-	
-	new->n = n;
+
+	new-> = n;
 	new->next = *head;
 	*head = new;
-	
+
 	return (*head);
 }
